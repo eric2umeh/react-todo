fb5a5207a23a0815adcd940d8f6dc1a877920074
Eric <eric2umeh@yahoo.com>
2023-02-20 10:45:58 +0100

    Create form for add, remove and check field

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import styles from '../styles/TodoItem.module.css';
+
+const TodoItem = ({ itemProp, setTodos, delTodo }) => {
+  const [editing, setEditing] = useState(false);
+
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const completedStyle = {
+    fontStyle: 'italic',
+    color: '#595959',
+    opacity: 0.4,
+    textDecoration: 'line-through',
+  };
+
+  const handleEditing = () => {
+    setEditing(true);
+  };
+
+  return (
+    <li className={styles.item}>
+      <div className={styles.content}>
+        <input
+          type="checkbox"
+          checked={itemProp.completed}
+          onChange={() => handleChange(itemProp.id)}
+        />
+        <button onClick={handleEditing}>Edit</button>
+        <button onClick={() => delTodo(itemProp.id)}>Delete</button>
+        <span style={itemProp.completed ? completedStyle : null}>
+          {itemProp.title}
+        </span>
+      </div>
+    </li>
+  );
+};
+export default TodoItem;
